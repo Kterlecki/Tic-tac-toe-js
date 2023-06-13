2a21a65beb89c0a5cf7a5bda42b4354c8cc1558a
Kterlecki <terlecki1995@gmail.com>
2023-06-13 08:36:27 +0100

    Initial working build

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+let currentPlayer = "X";
+
+function handleCellClick(event){
+    const cell = event.target
+
+    if(!cell.textContent.trim())
+    {
+        cell.textContent = currentPlayer;
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+}
