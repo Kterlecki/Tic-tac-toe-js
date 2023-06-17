cfa7d2da4c80ea400e3c2eed3c47334477936893
Kterlecki <terlecki1995@gmail.com>
2023-06-17 16:02:00 +0100

    Game reset functionality

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function updateCurrentPlayerText(){
     const currentPlayerElement = document.getElementById("currentPlayer");
     currentPlayerElement.textContent = "Current Player :" + currentPlayer
 }
+
+function resetGame(){
+    const cells = document.querySelectorAll("table td");
+    cells.forEach((cell) => {
+        cell.textContent = "";
+    });
+
+    currentPlayer = "X";
+    updateCurrentPlayerText();
+}
+// reset button for the grid so thhat you can re enter stuff
