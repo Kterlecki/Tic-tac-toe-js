a1b278055e193ca185e4dacd9bb1d6227816d539
Kterlecki <terlecki1995@gmail.com>
2023-06-18 09:18:28 +0100

    Add handling win and draw, score for players and its updates methods

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let currentPlayer = "X";
+let playerXScore = 0;
+let playerOScore = 0;
 
 function handleCellClick(event){
     const cell = event.target
@@ -7,13 +9,14 @@ function handleCellClick(event){
     {
         cell.textContent = currentPlayer;
         currentPlayer = currentPlayer === "X" ? "O" : "X";
+        scoreCheck();
         updateCurrentPlayerText();
     }
 }
 
 function updateCurrentPlayerText(){
     const currentPlayerElement = document.getElementById("currentPlayer");
-    currentPlayerElement.textContent = "Current Player :" + currentPlayer
+    currentPlayerElement.textContent = "Current Player: " + currentPlayer;
 }
 
 function resetGame(){
@@ -25,4 +28,53 @@ function resetGame(){
     currentPlayer = "X";
     updateCurrentPlayerText();
 }
-// reset button for the grid so thhat you can re enter stuff
+
+function scoreCheck(){
+    const cells = document.querySelectorAll("table td");
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+    for (let combination of winningCombinations){
+        const [a, b, c ] = combination;
+
+        if(
+            cells[a].textContent === cells[b].textContent &&
+            cells[a].textContent === cells[c].textContent &&
+            cells[a].textContent.trim() !== ""
+        ) {
+            handleWin(cells[a].textContent);
+            return;
+        }
+    }
+    if (Array.from(cells).every((cell) => cell.textContent.trim() !== "")) {
+        handleDraw();
+    }
+    
+}
+
+function handleDraw(){
+    alert("Its a draw!");
+    resetGame();
+}
+
+function handleWin(winningPlayer){
+
+    if(winningPlayer === "X"){
+        playerXScore++;
+    } else if (winningPlayer === "O"){
+        playerOScore++;
+    }
+    alert("Player " + winningPlayer + " wins!");
+    updateScore();
+    resetGame();
+}
+
+function updateScore(){
+    const playerXScorePageElement = document.getElementById("playerXScore");
+    const playerOScorePageElement = document.getElementById("playerOScore");
+
+    playerXScorePageElement.textContent = "Player X score: " + playerXScore;
+    playerOScorePageElement.textContent = "Player O score: " + playerOScore;
+}
